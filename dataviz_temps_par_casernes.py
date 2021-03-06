996a12a0052b2ef2e44ef4e42ab41d24bffce920
Coulandreau <79855919+Coulandreau@users.noreply.github.com>
2021-03-06 18:49:42 +0100

    create dataviz_temps_par_casernes

---
+++ dataviz_temps_par_casernes.py
@@ -0,0 +1,57 @@
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+# Chargement du dataset des incidents
+df = pd.read_excel('LFB Incident data from January 2017.xlsx')
+
+
+# Création nouveau dataframe de travail dfw  en retirant les colonnes inutiles pour la dataviz
+dfw = df.drop(['Postcode_full', 'UPRN', 'USRN', 'IncGeo_BoroughCode', 'ProperCase', 'IncGeo_WardCode', 'Easting_m', 'Northing_m',
+       'Easting_rounded', 'Northing_rounded', 'FRS', 'NumStationsWithPumpsAttending', 'NumPumpsAttending', 'PumpCount',
+       'IncGeo_WardNameNew', 'Latitude', 'Longitude', 'PumpHoursRoundUp', 'Notional Cost (£)'], axis=1)
+
+# utilisation du script pour détecter les colonnes avec des valeurs manquantes
+# def valeur_manquante(df):
+#     flag=0
+#     for col in df.columns:
+#             if df[col].isna().sum() > 0:
+#                 flag=1
+#                 print(f'"{col}": {df[col].isna().sum()} valeurs manquantes')
+#     if flag==0:
+#         print("Le dataset ne contient plus de valeurs manquantes, bien joué.")
+# valeur_manquante(dfw)
+
+# suppression des lignes avec temps d'arrivée manquant et des lignes avec caserne d'origine manquante
+dfw.dropna(subset = ['FirstPumpArriving_AttendanceTime'], axis=0, inplace = True)
+dfw.dropna(subset = ['FirstPumpArriving_DeployedFromStation'], axis=0, inplace = True)
+
+# Récupération des noms des casernes les plus rapides et les plus lentes
+# on ne prend que les incidents pour lesquels la caserne qui est intervenue est celle du secteur de l'incident pour ne pas défavoriser les casernes intervenues ailleurs
+res = pd.DataFrame(dfw[dfw.IncidentStationGround == dfw.FirstPumpArriving_DeployedFromStation].groupby(['FirstPumpArriving_DeployedFromStation'], as_index = False).agg({'FirstPumpArriving_AttendanceTime' : 'median'}) )
+print("Récupération des 3 casernes les plus rapides :\n")
+display(res.sort_values('FirstPumpArriving_AttendanceTime', ascending=True).head(3))
+print("Récupération des 3 casernes les plus lentes :\n")
+display(res.sort_values('FirstPumpArriving_AttendanceTime', ascending=False).head(3))
+
+
+# récupération des infos pour la dataviz dans res_h
+res_h = pd.DataFrame(dfw[dfw.IncidentStationGround == dfw.FirstPumpArriving_DeployedFromStation].groupby(['FirstPumpArriving_DeployedFromStation', 'HourOfCall'], as_index = False).agg({'FirstPumpArriving_AttendanceTime' : 'mean'}) )
+
+# Dataviz Temps d'arrivée moyen selon l'heure de la journée par caserne (3 plus rapides vs 3 plus lentes)
+plt.figure(figsize=(6,20))
+sns.relplot(x = 'HourOfCall', y = 'FirstPumpArriving_AttendanceTime',kind='line', 
+            palette = 'viridis', height=8,
+            data = res_h[res_h.FirstPumpArriving_DeployedFromStation.isin(['Orpington', 'Wennington', 'Ruislip', 'Lewisham', 'Brixton', 'Whitechapel'])], 
+            hue = 'FirstPumpArriving_DeployedFromStation' )
+plt.title('Temps d\'arrivée moyen selon l\'heure de la journée par caserne (3 plus rapides vs 3 plus lentes)')
+plt.xlabel('Heure d\'appel')
+plt.ylabel('Temps d\'arrivée moyen (minutes)')
+plt.yticks([240, 300, 360, 420, 480, 540], [4, 5, 6, 7, 8, 9])
+plt.xticks([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24], ['0','2','4','6','8','10','Midi','14','16','18','20','22','Minuit'])
+;
+
+
