deda346d7753b4bb51d1e4601e6c052a7ba4ed3f
arnaud hubert <51398701+pad-awan@users.noreply.github.com>
2021-03-06 18:51:53 +0100

    data_merged _cleaning_short
    data_merged _cleaning_short

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,33 +8,21 @@ Created on Fri Mar  5 23:13:18 2021
 import pandas as pd
 from datetime import datetime as dt
 
-
 debut = dt.today()
-incident = pd.read_excel("LFB_Incident_January 2017_2021.xlsx",sheet_name='Sheet1',index_col=0)
-incident.head()
 
-fin = dt.today()
-print(fin - debut)
-
-mobilisation = pd.read_excel("LFB Mobilisation data from January 2017.xlsx")
-df_merged = incident.merge(mobilisation)
-# Informations sur df_merged
-df_merged.info()
-
-
-# test si inc Geo_WardName même valeur que IncGeo_WardNameNew
-sorted(df_merged['IncGeo_WardName']) == sorted(df_merged['IncGeo_WardNameNew'])
-
-#### Avant suppression des variables
-
-to_drop = ['ResourceMobilisationId','Resource_Code','TimeMobileTimezoneId',
-           'TimeArrivedTimezoneId','TimeLeftTimezoneId','DateAndTimeReturned',
-           'TimeReturnedTimezoneId','DeployedFromLocation','PlusCode_Code',
-           'PlusCode_Description','CalYear','Postcode_full','UPRN','USRN',
-           'IncGeo_BoroughName','IncGeo_WardName','Easting_m','Northing_m',
-           'Easting_rounded','Northing_rounded','FRS']
-
-new_df_merged = df_merged.drop(to_drop,axis=1)
+usecols1= ['IncidentNumber','DateOfCall','TimeOfCall','IncidentGroup','StopCodeDescription','SpecialServiceType','PropertyCategory' ,'PropertyType',
+          'AddressQualifier','IncGeo_BoroughCode','IncGeo_WardNameNew', 'IncGeo_WardCode','ProperCase','Latitude','Longitude','IncidentStationGround',
+          'FirstPumpArriving_DeployedFromStation','SecondPumpArriving_DeployedFromStation','FirstPumpArriving_AttendanceTime',
+          'SecondPumpArriving_AttendanceTime' ,'NumStationsWithPumpsAttending','NumPumpsAttending','PumpCount',
+          'PumpHoursRoundUp','Notional Cost (£)']
+file1 = 'LFB Incident data from January 2017.xlsx'
+incident = pd.read_excel(io=file1,usecols=usecols1 , sheet_name=0, index_col=0 ,date_parser='DateOfCall')
+# incident = pd.read_excel("LFB_Incident_January 2017_2021.xlsx",sheet_name='Sheet1',index_col=0)
+file2 = "LFB Mobilisation data from January 2017.xlsx"
+usecols2=['IncidentNumber','DateAndTimeMobilised','DateAndTimeMobile','DateAndTimeArrived','DateAndTimeLeft',
+         'DeployedFromStation_Code','DelayCodeId','DeployedFromStation_Name']
+mobilisation = pd.read_excel(io=file2,index_col=0,usecols = usecols2)
+df_merged = incident.merge(mobilisation,how='left',on='IncidentNumber')
 
 def valeur_manquante(df):
     flag=0
@@ -44,64 +32,52 @@ def valeur_manquante(df):
                 print(f'"{col}": {df[col].isna().sum()} valeurs manquantes')
     if flag==0:
         print("Le dataset ne contient plus de valeurs manquantes, bien joué.")
-valeur_manquante(new_df_merged)
 
-# Attention filtre sur les notnull supprime environ 150000 lignes dans un new df
-new_df_merged = new_df_merged[new_df_merged['Latitude'].notnull()]
-# colonnes en doublons après la fusion à supprimer
-to_drop = ['PerformanceReporting' ,'DateAndTimeMobilised' ,'AttendanceTimeSeconds' ,'PumpOrder' ,'IncidentStationGround']
-new_df_merged = new_df_merged.drop(to_drop,axis=1)
 
-# set index
-new_df_merged = new_df_merged.set_index('IncidentNumber')
-new_df_merged['SpecialServiceType'] = new_df_merged['SpecialServiceType'].fillna('RAS')
+df_merged = df_merged[df_merged['Latitude'].notnull()]
+df_merged['SpecialServiceType'] = df_merged['SpecialServiceType'].fillna('RAS')
 
 # categoriser et typer les variables
 to_category = ['IncidentGroup','StopCodeDescription','PropertyCategory' ,'PropertyType' ,'AddressQualifier' ,
-               'Postcode_district' ,'IncGeo_BoroughCode' ,'ProperCase' ,
+               'IncGeo_BoroughCode' ,'ProperCase' ,
                'IncGeo_WardCode' ,'IncGeo_WardNameNew' ,'SpecialServiceType']
-new_df_merged[to_category] = new_df_merged[to_category].astype('category')
+df_merged[to_category] = df_merged[to_category].astype('category')
 
-valeur_manquante(new_df_merged)
-
-
-new_df_merged['DateAndTimeMobile'] = pd.to_datetime(new_df_merged['DateAndTimeMobile'], errors='coerce')
-new_df_merged['DateAndTimeArrived'] = pd.to_datetime(new_df_merged['DateAndTimeArrived'], errors='coerce')
-new_df_merged['DateAndTimeLeft'] = pd.to_datetime(new_df_merged['DateAndTimeLeft'], errors='coerce')
-
-valeur_manquante(new_df_merged)
+df_merged['DateAndTimeMobile'] = pd.to_datetime(df_merged['DateAndTimeMobile'], errors='coerce')
+df_merged['DateAndTimeArrived'] = pd.to_datetime(df_merged['DateAndTimeArrived'], errors='coerce')
+df_merged['DateAndTimeLeft'] = pd.to_datetime(df_merged['DateAndTimeLeft'], errors='coerce')
 
 # fillna 0
 to_fillna = ['PumpCount','PumpHoursRoundUp' ,'Notional Cost (£)' ,'DelayCodeId',
              'SecondPumpArriving_AttendanceTime','FirstPumpArriving_AttendanceTime',
-             'DateAndTimeMobile','DateAndTimeLeft' ,'DeployedFromStation_Code' ,'DeployedFromStation_Name']
-new_df_merged[to_fillna] = new_df_merged[to_fillna].fillna(0)
-
-valeur_manquante(new_df_merged)
-
+             'DateAndTimeMobile','DateAndTimeLeft' ,'DeployedFromStation_Code' ,
+             'DeployedFromStation_Name','NumStationsWithPumpsAttending','NumPumpsAttending']
+df_merged[to_fillna] = df_merged[to_fillna].fillna(0)
 
 # Remplacer les valeurs par 'no'
 
-to_fill_no = ['FirstPumpArriving_DeployedFromStation','SecondPumpArriving_DeployedFromStation','DelayCode_Description']
-new_df_merged[to_fill_no] = new_df_merged[to_fill_no].fillna('no')
+to_fill_no = ['FirstPumpArriving_DeployedFromStation','SecondPumpArriving_DeployedFromStation']
+df_merged[to_fill_no] = df_merged[to_fill_no].fillna('no')
 
-new_df_merged['FirstPumpArriving_DeployedFromStation','SecondPumpArriving_DeployedFromStation'].astype('category')
 
-cat_data = new_df_merged.select_dtypes(include=['O','category'])
+df_merged[to_fill_no] = df_merged[to_fill_no].astype('category')
+df_merged.info()
+cat_data = df_merged.select_dtypes(include=['O','category'])
 print(len(cat_data.columns),"colonnes de type objets")
 cat_data.columns
 
 
-num_data = new_df_merged.select_dtypes(include=['float64' ,'int64'])
+num_data = df_merged.select_dtypes(include=['float64' ,'int64'])
 print(len(num_data.columns),"colonnes numériques")
 num_data.columns
 
-date_data = new_df_merged.select_dtypes(include=['datetime','datetime64'])
+date_data = df_merged.select_dtypes(include=['datetime','datetime64'])
 print(len(date_data.columns),"colonnes date")
 date_data.columns
 
 
-valeur_manquante(new_df_merged)
+valeur_manquante(df_merged)
+df_merged.info()
 # timing fin
 fin = dt.today()
 print(fin - debut)
