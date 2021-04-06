9ec2db00f18afad0b184f77a2edaa8f035285e08
arnaud hubert <arnaudhub@yahoo.fr>
2021-04-06 22:56:39 +0200

    update
    update with more fonctions and more options on classifiers and conditions

--- rescue_time_prediction3.py
+++ rescue_time_prediction3.py
@@ -8,21 +8,44 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.metrics import accuracy_score
+# from sklearn.metrics import accuracy_score
+import time
+# st.echo()
+st.set_page_config(page_title='London Fire Brigade - Rescue Times', 
+                   page_icon=None, 
+                   layout='centered', 
+                   initial_sidebar_state='auto')
+# st.title('London Fire Brigade - Rescue Times')
 
-st.title('London Fire Brigade - Rescue Times')
+st.date_input("date de présentation:")
 
-
+st.write("""
+          Prédiction du temps de réponse d’un véhicule de la 
+Brigade des Pompiers de Londres
+         L’objectif de ce projet est d’analyser et/ou d’estimer les temps de réponse et de mobilisation de la Brigade 
+des Pompiers de Londres (London Fire Brigade, LFB). La brigade des pompiers de Londres est le service 
+d'incendie et de sauvetage le plus actif du Royaume-Uni et l'une des plus grandes organisations de lutte 
+contre l'incendie et de sauvetage au monde.
+            """)
 
 st.sidebar.header('Settings')
 ####### Arnaud
 
 st.write("""
-         quel est le meilleur modèle ?
+         Quel est le meilleur modèle ?
          """)
 classifier_name = st.sidebar.selectbox("Sélectionner le modèle dans la sidebar", ("LinearRegression","GradientBoostingRegressor"))
 
-st.button("Selectionner le modèle")
+model = st.button("Selectionner le modèle")
+def run_status():
+    latest_iteration = st.empty()
+    bar = st.progress(0)
+    for i in range(100):
+        latest_iteration.text(f'Percent complete {i+1}')
+        bar.progress(i+1)
+        time.sleep(0.1)
+        st.empty()
+
 
 
 def add_parameter_ui(clf_name):
@@ -31,10 +54,15 @@ def add_parameter_ui(clf_name):
         n_jobs = st.sidebar.slider("n_jobs",10,500)
         params["n_jobs"] = n_jobs
     else:
-        # loss = st.sidebar.slider("loss",["ls", "lad", "huber", "quantile"])
-        n_estimators = st.sidebar.slider("n_estimators",1,100)
-        # params["loss"] = loss
+        loss = st.sidebar.selectbox('loss:',["lad"])
+        n_estimators = st.sidebar.slider("n_estimators",100,10000)
+        # alpha=st.sidebar.slider("alpha",1,10)
+        max_depth = st.sidebar.slider("max_depth",1,5)
+        # params["alpha"] = alpha
+        params["loss"] = loss
         params["n_estimators"] = n_estimators
+        params["max_depth"] = max_depth
+        # params["learning_rate"] = learning_rate
     return params
 
 params = add_parameter_ui(classifier_name)
@@ -44,7 +72,7 @@ def get_classifier(clf_name,params):
     if clf_name=="LinearRegression":
         clf = LinearRegression(n_jobs=params["n_jobs"],normalize=True)
     elif clf_name =="GradientBoostingRegressor":
-        clf = GradientBoostingRegressor(n_estimators=params["n_estimators"],)
+        clf = GradientBoostingRegressor(n_estimators=params["n_estimators"],loss=params["loss"])
     return clf
 
 clf = get_classifier(classifier_name, params)
@@ -58,8 +86,11 @@ df_cas_quart = df_cas_quart.sort_values('ProperCase', ascending = True)
 
 
 # GPS borough points load
-gps = pd.read_csv('gpsfinal.csv', index_col=0, sep=";")
 
+def upload_gps():
+    gps = pd.read_csv('gpsfinal.csv', index_col=0, sep=";")
+    return gps
+gps = upload_gps()
 
 # Sidebar Borough name list -  load
 liste_qu = []
@@ -82,7 +113,7 @@ df_codeid = pd.read_csv('df_codeid_2019.csv', index_col=0)
 liste_delaycodeid = []
 for description in df_codeid.DelayCode_Description.unique():
     liste_delaycodeid.append(description)
-
+# st.write(liste_delaycodeid)
 
 # SpecialServiceType and description - load
 df_sst = pd.read_csv('df_sst_2019.csv', index_col=0)
@@ -92,8 +123,12 @@ for sst in df_sst.SpecialServiceType.unique():
     liste_sst.append(sst)
 
 # num_data_2020 preprocessed load
-num_data = pd.read_csv('num_data_2020.csv', index_col=0)
-num_data2 = num_data
+
+def upload_data2020():
+    num_data = pd.read_csv('num_data_2020.csv', index_col=0)
+    num_data2 = num_data.copy()
+    return num_data
+num_data = upload_data2020()
 
 # Ward + incident group + weekday - load
 df_sqigwd = pd.read_csv('df_sqigwd_2019.csv', index_col=0)
@@ -108,7 +143,8 @@ df_sqid = pd.read_csv('df_sqid_2019.csv', index_col=0)
 # Sidebar Settings retrieving function
 def user_input():
     hourofcall = st.sidebar.slider('Hour of Call', 0, 23, 12)
-    weekday = st.sidebar.select_slider('Weekday', options=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+    weekday = st.sidebar.select_slider('Weekday', 
+                                       options=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
     incidentgroup = st.sidebar.radio('Incident Group', ('Fire', 'False Alarm', 'Special Service'))
     sst_sel= st.sidebar.selectbox('Special Service Type', liste_sst)
     codeid_description= st.sidebar.selectbox('DelayCode Description', liste_delaycodeid)
@@ -212,26 +248,54 @@ num_data_pred['tmoyq'] = df_cas_quart[df_cas_quart.ProperCase_num == val_quartie
 num_data_pred['tmedq'] = df_cas_quart[df_cas_quart.ProperCase_num == val_quartier][['tmedq']].values
 #  2019 Statistics retrieval when exists for this Wardname / incidentgroup / weekday
 if df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)&(df_sqigwd.IncidentGroup == df.incidentgroup[0])&(df_sqigwd.CalWeekDay == num_wd)].shape[0] == 1:
-    num_data_pred['casernes_sous_quartier'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)&(df_sqigwd.IncidentGroup == df.incidentgroup[0])&(df_sqigwd.CalWeekDay == num_wd)][['casernes_sous_quartier']].values
-    num_data_pred['tmoysqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)&(df_sqigwd.IncidentGroup == df.incidentgroup[0])&(df_sqigwd.CalWeekDay == num_wd)][['tmoysqigwd']].values
-    num_data_pred['tmedsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)&(df_sqigwd.IncidentGroup == df.incidentgroup[0])&(df_sqigwd.CalWeekDay == num_wd)][['tmedsqigwd']].values
-    num_data_pred['tstdsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)&(df_sqigwd.IncidentGroup == df.incidentgroup[0])&(df_sqigwd.CalWeekDay == num_wd)][['tstdsqigwd']].values
-    num_data_pred['tminsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)&(df_sqigwd.IncidentGroup == df.incidentgroup[0])&(df_sqigwd.CalWeekDay == num_wd)][['tminsqigwd']].values
-    num_data_pred['tmaxsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)&(df_sqigwd.IncidentGroup == df.incidentgroup[0])&(df_sqigwd.CalWeekDay == num_wd)][['tmaxsqigwd']].values
+    num_data_pred['casernes_sous_quartier'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)
+                                                        &(df_sqigwd.IncidentGroup == df.incidentgroup[0])
+                                                        &(df_sqigwd.CalWeekDay == num_wd)][['casernes_sous_quartier']].values
+    num_data_pred['tmoysqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)
+                                            &(df_sqigwd.IncidentGroup == df.incidentgroup[0])
+                                            &(df_sqigwd.CalWeekDay == num_wd)][['tmoysqigwd']].values
+    num_data_pred['tmedsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)
+                                            &(df_sqigwd.IncidentGroup == df.incidentgroup[0])
+                                            &(df_sqigwd.CalWeekDay == num_wd)][['tmedsqigwd']].values
+    num_data_pred['tstdsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)
+                                            &(df_sqigwd.IncidentGroup == df.incidentgroup[0])
+                                            &(df_sqigwd.CalWeekDay == num_wd)][['tstdsqigwd']].values
+    num_data_pred['tminsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)
+                                            &(df_sqigwd.IncidentGroup == df.incidentgroup[0])
+                                            &(df_sqigwd.CalWeekDay == num_wd)][['tminsqigwd']].values
+    num_data_pred['tmaxsqigwd'] = df_sqigwd[(df_sqigwd.IncGeo_WardName_num == val_sous_quartier)
+                                            &(df_sqigwd.IncidentGroup == df.incidentgroup[0])
+                                            &(df_sqigwd.CalWeekDay == num_wd)][['tmaxsqigwd']].values
 # 2019 Statistics retrieval when exists for this  Boroughname / SpecialServiceType / HourOfCall
 if df_quart[(df_quart.ProperCase == df.quartier[0])&(df_quart.SpecialServiceType == df.specialservicetype[0])&(df_quart.HourOfCall == num_heure)].shape[0] == 1:
-    num_data_pred['tmoyqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])&(df_quart.SpecialServiceType == df.specialservicetype[0])&(df_quart.HourOfCall == num_heure)][['tmoyqussho']].values
-    num_data_pred['tmedqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])&(df_quart.SpecialServiceType == df.specialservicetype[0])&(df_quart.HourOfCall == num_heure)][['tmedqussho']].values
-    num_data_pred['tstdqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])&(df_quart.SpecialServiceType == df.specialservicetype[0])&(df_quart.HourOfCall == num_heure)][['tstdqussho']].values
-    num_data_pred['tminqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])&(df_quart.SpecialServiceType == df.specialservicetype[0])&(df_quart.HourOfCall == num_heure)][['tminqussho']].values
-    num_data_pred['tmaxqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])&(df_quart.SpecialServiceType == df.specialservicetype[0])&(df_quart.HourOfCall == num_heure)][['tmaxqussho']].values
+    num_data_pred['tmoyqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])
+                                           &(df_quart.SpecialServiceType == df.specialservicetype[0])
+                                           &(df_quart.HourOfCall == num_heure)][['tmoyqussho']].values
+    num_data_pred['tmedqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])
+                                           &(df_quart.SpecialServiceType == df.specialservicetype[0])
+                                           &(df_quart.HourOfCall == num_heure)][['tmedqussho']].values
+    num_data_pred['tstdqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])
+                                           &(df_quart.SpecialServiceType == df.specialservicetype[0])
+                                           &(df_quart.HourOfCall == num_heure)][['tstdqussho']].values
+    num_data_pred['tminqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])
+                                           &(df_quart.SpecialServiceType == df.specialservicetype[0])
+                                           &(df_quart.HourOfCall == num_heure)][['tminqussho']].values
+    num_data_pred['tmaxqussho'] = df_quart[(df_quart.ProperCase == df.quartier[0])
+                                           &(df_quart.SpecialServiceType == df.specialservicetype[0])
+                                           &(df_quart.HourOfCall == num_heure)][['tmaxqussho']].values
 # 2019 Statistics retrieval when exists for this Wardname / DelayCodeId
-if df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)&(df_sqid.DelayCodeId == val_codeid)].shape[0] == 1:
-    num_data_pred['tmoyCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)&(df_sqid.DelayCodeId == val_codeid)][['tmoyCodeId']].values
-    num_data_pred['tmedCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)&(df_sqid.DelayCodeId == val_codeid)][['tmedCodeId']].values
-    num_data_pred['tstdCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)&(df_sqid.DelayCodeId == val_codeid)][['tstdCodeId']].values
-    num_data_pred['tminCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)&(df_sqid.DelayCodeId == val_codeid)][['tminCodeId']].values
-    num_data_pred['tmaxCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)&(df_sqid.DelayCodeId == val_codeid)][['tmaxCodeId']].values
+if df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)
+           &(df_sqid.DelayCodeId == val_codeid)].shape[0] == 1:
+    num_data_pred['tmoyCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)
+                                          &(df_sqid.DelayCodeId == val_codeid)][['tmoyCodeId']].values
+    num_data_pred['tmedCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)
+                                          &(df_sqid.DelayCodeId == val_codeid)][['tmedCodeId']].values
+    num_data_pred['tstdCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)
+                                          &(df_sqid.DelayCodeId == val_codeid)][['tstdCodeId']].values
+    num_data_pred['tminCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)
+                                          &(df_sqid.DelayCodeId == val_codeid)][['tminCodeId']].values
+    num_data_pred['tmaxCodeId'] = df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)
+                                          &(df_sqid.DelayCodeId == val_codeid)][['tmaxCodeId']].values
 # features line display
 #st.write(num_data_pred)
 # num_data 2020 and features dataframe Normalization
@@ -241,32 +305,29 @@ if df_sqid[(df_sqid.IncGeo_WardName == sous_quartier)&(df_sqid.DelayCodeId == va
 # from sklearn.metrics import mean_squared_error
 
 scaler = preprocessing.StandardScaler().fit(num_data)
-num_data[num_data.columns] = pd.DataFrame(scaler.transform(num_data), columns=num_data.columns, index= num_data.index)
-num_data_pred[num_data_pred.columns] = pd.DataFrame(scaler.transform(num_data_pred), columns=num_data_pred.columns, index= num_data_pred.index)
+num_data[num_data.columns] = pd.DataFrame(scaler.transform(num_data), 
+                                          columns=num_data.columns, 
+                                          index= num_data.index)
+num_data_pred[num_data_pred.columns] = pd.DataFrame(scaler.transform(num_data_pred),
+                                                    columns=num_data_pred.columns, 
+                                                    index= num_data_pred.index)
     
-
-
-
-
-
 ###Classification
 dfw = num_data
 target = dfw.FirstPumpArriving_AttendanceTime
 feats = dfw.drop(['FirstPumpArriving_AttendanceTime'], axis=1)
 # 2020 samples
 
+# split datas
 X_train, X_test, y_train, y_test = train_test_split(feats, target, test_size=0.2,random_state=1234)
 clf.fit(X_train, y_train)
 
 y_pred = clf.score(X_test,y_test)
-# st.write("y_test",y_test.shape)
 
-# st.write("y_pred:",y_pred.shape)
-
-# st.write("y_pred:",y_pred.shape)
-# acc = accuracy_score(y_test,y_pred)
-# acc = accuracy_score(y_test, y_pred)
+# display the classifier
 st.write(f"classifier ={classifier_name}")
+
+# display score of the regression model
 st.write(f"score = {y_pred}")
 
 # delete target column from num_data_pred into feats_demo
@@ -274,6 +335,9 @@ feats_demo = num_data_pred.drop(['FirstPumpArriving_AttendanceTime'], axis=1)
 # prediction with trained model
 pred_test = clf.predict(feats_demo)
 
+
+# def results():
+# feats_demo,gps
 # reconstruction of the results 
 num_X1 = feats_demo[['HourOfCall']]
 num_X2 = feats_demo[['DelayCodeId', 'ProperCase_num',
@@ -300,6 +364,12 @@ tps_seconds = tps_estimé % 60
 #success_message = 'Estimated LFB arrival time : ' + str(tps_estimé) + ' seconds.'
 success_message = 'Estimated LFB arrival time : ' + str(tps_minutes) + ' minutes ' + str(tps_seconds) + ' seconds.'
 
+
+# Estimated LFB arrival time display
+st.success(success_message)
+
+
+
 #Loading of the data to create the map
 datagps=(df.quartier)
 mapgps=gps.merge(right=datagps, on = "quartier", how = "right")
@@ -308,49 +378,51 @@ mapgps['min']=tps_minutes
 mapgps['sec']=tps_seconds
 
 
+# results()
+
+def display_map():
+    global min,sec,quartier,mapgps
 
 
+    #Display of the map
+    tooltip = {
+        "html": "The estimated arrival time is <b>{min}</b> min <b>{sec}</b> sec<br> for <b>{quartier}</b> area,<br> depending the information entered.",
+        "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+    }
+    
+    st.pydeck_chart(pdk.Deck(
+        map_style='mapbox://styles/cedric60128/ckn0g3q6u13pz17pbnp9vmn6n',
+        tooltip = tooltip,
+        initial_view_state=pdk.ViewState(
+        latitude=51.495,
+        longitude=-0.120,
+        zoom=8.80,
+        pitch=50,
+        bearing=-30
+    ),
+        layers=[
+            pdk.Layer(
+                'ColumnLayer',
+                data=mapgps,
+                get_position='[lon, lat]',
+                get_elevation='[temps]',
+                radius=500,
+                auto_highlight=True,
+                elevation_scale=50,
+                elevation_range=[0, 100],
+                get_fill_color=["temps * -100", "temps", "temps * 10", 300],
+                pickable=True,
+                extruded=True
+            )
+        ]
+    ))
+map_active = st.button("Afficher la carte")
+# if map_active :
+display_map()
 
+if model :
+    run_status()
 
+# if map_active:
+#     run_status()
 
-
-
-
-
-
-
-
-#Display of the map
-tooltip = {
-    "html": "The estimated arrival time is <b>{min}</b> min <b>{sec}</b> sec<br> for <b>{quartier}</b> area,<br> depending the information entered.",
-    "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
-}
-
-st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/cedric60128/ckn0g3q6u13pz17pbnp9vmn6n',
-    tooltip = tooltip,
-    initial_view_state=pdk.ViewState(
-    latitude=51.495,
-    longitude=-0.120,
-    zoom=8.80,
-    pitch=50,
-    bearing=-30
-),
-    layers=[
-        pdk.Layer(
-            'ColumnLayer',
-            data=mapgps,
-            get_position='[lon, lat]',
-            get_elevation='[temps]',
-            radius=500,
-            auto_highlight=True,
-            elevation_scale=50,
-            elevation_range=[0, 100],
-            get_fill_color=["temps * -100", "temps", "temps * 10", 300],
-            pickable=True,
-            extruded=True
-        )
-    ]
-))
-# Estimated LFB arrival time display
-st.success(success_message)
